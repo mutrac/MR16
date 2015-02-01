adbbf32f664b64bd856a1bbf8885b03e8207ae7d
Trevor Stanhope <trevor@firebase>
2015-02-01 04:13:44 -0500

    big revisions for subsys

--- subsystems/SBS/SBS.ino
+++ subsystems/SBS/SBS.ino
@@ -1,58 +1,54 @@
 /*
-  Power Steering Subsystem (PSS)
+  Steering and Ballast Subsystem (SBS)
   McGill ASABE Tractor Team
 */
 
 /* --- Libraries --- */
+#include "PID_v1.h"
 
 /* --- Global Constants --- */
 const int BAUD = 57600;
-const int BUFFER_SIZE = 256;
+const int DATA_SIZE = 128;
+const int OUTPUT_SIZE = 256;
 const int STEERING_POSITION_PIN = A0;
 const int ACTUATOR_POSITION_PIN = A1;
+const int SUSPENSION_POSITION_PIN = A2;
+const int CART_POSITION_PIN = A3;
 
 /* --- Global Variables --- */
-int steering_position = 0;
-int actuator_position = 0;
-char output_buffer[BUFFER_SIZE];
+int STR_POS = 0;
+int ACT_POS = 0;
+char OUTPUT_BUFFER[OUTPUT_SIZE];
+int SUSP_POS = 0;
+int CART_POS = 0;
+char DATA_BUFFER[DATA_SIZE];
 
 /* --- Setup --- */
 void setup() {
   Serial.begin(BAUD);
   pinMode(STEERING_POSITION_PIN, INPUT);
   pinMode(ACTUATOR_POSITION_PIN, INPUT);
-}
-
-/* --- Loop --- */
-void loop() {
-  steering_position = analogRead(STEERING_POSITION_PIN);
-  actuator_position = analogRead(ACTUATOR_POSITION_PIN);
-  sprintf(output_buffer, "steering_position:%d, actuator_position:%d", steering_position, actuator_position);
-  Serial.println(output_buffer);
-}
-
-
-/* --- Global Constants --- */
-const int BAUD = 57600;
-const int BUFFER_SIZE = 256;
-const int LOAD_CELL_PIN = A0;
-const int CART_POSITION_PIN = A1;
-
-/* --- Global Variables --- */
-int load_cell = 0;
-int cart_position = 0;
-char output_buffer[BUFFER_SIZE];
-
-/* --- Setup --- */
-void setup() {
-  Serial.begin(BAUD);
-  pinMode(LOAD_CELL_PIN, INPUT);
+  pinMode(SUSPENSION_POSITION_PIN, INPUT);
   pinMode(CART_POSITION_PIN, INPUT);
 }
 
 /* --- Loop --- */
 void loop() {
-  cart_position = analogRead(CART_POSITION_PIN);
-  sprintf(output_buffer, "cart_position:%d, load_cell:%d", cart_position, load_cell);
-  Serial.println(output_buffer);
+  STR_POS = analogRead(STEERING_POSITION_PIN);
+  ACT_POS = analogRead(ACTUATOR_POSITION_PIN);
+  CART_POS = analogRead(CART_POSITION_PIN);
+  SUSP_POS = analogRead(SUSPENSION_POSITION_PIN);
+  sprintf(DATA_BUFFER, "[str_pos:%d,act_posi:%d,cart_pos:%d,susp_pos:%d]", STR_POS, ACT_POS, CART_POS, SUSP_POS);
+  sprintf(OUTPUT_BUFFER, "'{data':%s,'chksum':%d}", DATA_BUFFER, checksum());
+  Serial.println(OUTPUT_BUFFER);
+}
+
+/* --- Check Sum --- */
+int checksum() {
+  int sum = 0;
+  for (int i = 0; i < DATA_SIZE; i++) {
+    sum += DATA_BUFFER[i];
+  }
+  int val = sum % 256;
+  return val;
 }
--- subsystems/TCS/TCS.ino
+++ subsystems/TCS/TCS.ino
@@ -1,11 +1,38 @@
 /*
-  Variable Transmission Subsystem
+  Traction Control Subsystem (TCS)
+  McGill ASABE Tractor Team
 */
+
+/* --- Libraries --- */
+#include "PID_v1.h"
+
+/* --- Global Constants --- */
+const int BAUD = 57600;
+const int DATA_SIZE = 128;
+const int OUTPUT_SIZE = 256;
+
+/* --- Global Variables --- */
+char OUTPUT_BUFFER[OUTPUT_SIZE];
+char DATA_BUFFER[DATA_SIZE];
 
 /* --- Setup --- */
 void setup() {
+  Serial.begin(BAUD);
 }
 
 /* --- Loop --- */
 void loop() {
+  sprintf(DATA_BUFFER, "[]");
+  sprintf(OUTPUT_BUFFER, "'{data':%s,'chksum':%d}", DATA_BUFFER, checksum());
+  Serial.println(OUTPUT_BUFFER);
+}
+
+/* --- Check Sum --- */
+int checksum() {
+  int sum = 0;
+  for (int i = 0; i < DATA_SIZE; i++) {
+    sum += DATA_BUFFER[i];
+  }
+  int val = sum % 256;
+  return val;
 }
