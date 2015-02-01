61d0c57d6214e5b4db19bfb329e47327eadd9e4f
Trevor Stanhope <trevor@firebase>
2015-02-01 03:35:48 -0500

    renamed subsystems and added PID

--- subsystems/VSS/ESS.ino
+++ subsystems/VSS/ESS.ino
@@ -53,3 +53,36 @@ void loop() {
   sprintf(output_buffer, "ground_relay:%d, regulator_relay:%d, starter_relay:%d, brake_interlock:%d, cvt_interlock:%d, seat_killswitch:%d, hitch_killswitch:%d, button_killswitch:%d, ignition:%d", ground_relay, regulator_relay, starter_relay, brake_interlock, cvt_interlock, seat_killswitch, hitch_killswitch, button_killswitch, ignition);
   Serial.println(output_buffer);
 }
+
+/*
+  Electronic Braking Subsystem (EBS)
+  McGill ASABE Tractor Team
+*/
+
+/* --- Libraries --- */
+
+/* --- Global Constants --- */
+const int BAUD = 57600;
+const int BUFFER_SIZE = 256;
+const int LEFT_BRAKE_PIN = A0;
+const int RIGHT_BRAKE_PIN = A1;
+
+/* --- Global Variables --- */
+int right_brake = 0;
+int left_brake = 0;
+char output_buffer[BUFFER_SIZE];
+
+/* --- Setup --- */
+void setup() {
+  Serial.begin(BAUD);
+  pinMode(RIGHT_BRAKE_PIN, INPUT);
+  pinMode(LEFT_BRAKE_PIN, INPUT);
+}
+
+/* --- Loop --- */
+void loop() {
+  right_brake = analogRead(RIGHT_BRAKE_PIN);
+  left_brake = analogRead(LEFT_BRAKE_PIN);
+  sprintf(output_buffer, "left_brake:%d, right_brake:%d", left_brake, right_brake);
+  Serial.println(output_buffer);
+}
