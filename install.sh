97ac347305bb77c25c00e3405501e2bb72c54d16
Trevor Stanhope <trevor@firebase>
2015-02-25 04:00:15 -0500

    still need to add V6

--- install.sh
+++ install.sh
@@ -1,2 +1,25 @@
 #!/bin/sh
 # install.sh - Installation script for the MR16
+
+sudo apt-get update
+sudo apt-get upgrade
+
+# Python Base
+sudo apt-get install python-dev build-essential python-pip
+
+# Arduino
+sudo apt-get install arduino arduino-mk
+
+# MongoDB
+sudo apt-get install mongodb
+sudo pip install pymongo
+
+# CMQ
+sudo apt-get install zmq python-serial python-zmq
+
+# OBD
+sudo apt-get install python-cherrypy3
+
+# HUD
+sudo apt-get install python-tkinter
+
