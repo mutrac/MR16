bf1ec018ea2bfaf8cae1e70c85065328e5289cc1
Trevor Stanhope <trevor@W510>
2015-04-06 00:18:58 -0400

    semi-ready traction controller, manual and pull. manual --> CVT Absolute Position, pull --> CVT ratio

--- sketches/TCS/TCS.ino
+++ sketches/TCS/TCS.ino
@@ -23,20 +23,34 @@ const int ENCODER_INT = 3;
 const int ENCODER_A_PIN = 10;
 const int ENCODER_B_PIN = 11;
 
+// Digital Inputs
+const int CVT_MODE_PIN = 12;
+
 // Analog Input
-const int CVT_PIN = A0;
+const int CVT_POSITION_PIN = A0;
 
 /* --- Global Variables --- */
-// PID
-double K_P=1, K_I=0.05, K_D=0.25;
-double setpoint, input, output;
-PID cvt_pid(&input, &output, &setpoint, K_P, K_I, K_D, DIRECT);
+boolean PULL_MODE = false;
+int CVT_POSITION = 0;
+
+// Manual PID
+double MANUAL_P=1, MANUAL_I=0.05, MANUAL_D=0.25;
+double MANUAL_SET, MANUAL_IN, MANUAL_OUT;
+PID MANUAL_PID(&MANUAL_IN, &MANUAL_OUT, &MANUAL_SET, MANUAL_P, MANUAL_I, MANUAL_D, DIRECT);
+
+// Pull PID
+double PULL_P=1, PULL_I=0.05, PULL_D=0.25;
+double PULL_SET, PULL_IN, PULL_OUT;
+PID PULL_PID(&PULL_IN, &PULL_OUT, &PULL_SET, PULL_P, PULL_I, PULL_D, DIRECT);
+
+// Stepper
+AF_Stepper STEPPER(48, 2); // TODO: find stepper resolution
 
 // Counters
-volatile int sparkplug_pulses = 0;
-volatile int driveshaft_pulses = 0;
-volatile int wheel_pulses = 0;
-volatile int encoder_pulses = 0;
+volatile int SPARKPLUG_PULSES = 0;
+volatile int DRIVESHAFT_PULSES = 0;
+volatile int WHEEL_PULSES = 0;
+volatile int ENCODER_PULSES = 0;
 
 // Char Buffers
 char OUTPUT_BUFFER[OUTPUT_SIZE];
@@ -44,38 +58,86 @@ char DATA_BUFFER[DATA_SIZE];
 
 /* --- Setup --- */
 void setup() {
+  
   // Serial
   Serial.begin(BAUD);
   
   // Analog Inputs
-  pinMode(CVT_PIN, INPUT);
+  pinMode(CVT_POSITION_PIN, INPUT);
+  pinMode(CVT_MODE_PIN, INPUT);
+
+  // Initialize Manual PID
+  MANUAL_PID.SetMode(AUTOMATIC);
+  MANUAL_PID.SetTunings(MANUAL_P, MANUAL_I, MANUAL_D);
   
-  // Initialize PID
-  cvt_pid.SetMode(AUTOMATIC);
-  cvt_pid.SetTunings(K_P, K_I, K_D);
+  // Initialid Pull PID
+  PULL_PID.SetMode(AUTOMATIC);
+  PULL_PID.SetTunings(PULL_P, PULL_I, PULL_D);
   
   // Initilize asynch interrupts
   attachInterrupt(SPARKPLUG_INT, sparkplug_counter, RISING);
   attachInterrupt(DRIVESHAFT_INT, driveshaft_counter, RISING);
   attachInterrupt(WHEEL_INT, wheel_counter, RISING);
   attachInterrupt(ENCODER_INT, encoder_counter, RISING);
+  
+  // Initialize Stepper
+  STEPPER.setSpeed(100); // the stepper speed in rpm
 }
 
 /* --- Loop --- */
 void loop() {
-  // Reset looping counters (doesn't include the encoder)
-  sparkplug_pulses = 0;
-  driveshaft_pulses = 0;
-  wheel_pulses = 0;
   
-  // Wait and calculate
+  // Reset looping counters (doesn't include the encoder)
+  SPARKPLUG_PULSES = 0;
+  DRIVESHAFT_PULSES = 0;
+  WHEEL_PULSES = 0;
+  
+  // Wait and get values
   delay(INTERVAL);
-  int driveshaft_rpm =  driveshaft_pulses / INTERVAL;
-  int sparkplug_rpm =  sparkplug_pulses / INTERVAL;
-  int wheel_rpm =  wheel_pulses / INTERVAL;
-  input = encoder_pulses;
-  setpoint = analogRead(CVT_PIN);
-  cvt_pid.Compute();
+  int driveshaft_rpm =  DRIVESHAFT_PULSES / INTERVAL;
+  int sparkplug_rpm =  SPARKPLUG_PULSES / INTERVAL;
+  int wheel_rpm =  WHEEL_PULSES / INTERVAL;
+  int cvt_ratio = sparkplug_rpm / driveshaft_rpm;
+  int differential_ratio = driveshaft_rpm / wheel_rpm;
+  CVT_POSITION = analogRead(CVT_POSITION_PIN);
+  
+  // Calculate manual controller output
+  MANUAL_IN = double(ENCODER_PULSES);
+  MANUAL_SET = double(CVT_POSITION);
+  MANUAL_PID.Compute();
+  
+  // Calculate automatic controller output
+  PULL_IN = double(cvt_ratio);
+  PULL_SET = double(CVT_POSITION);
+  PULL_PID.Compute();
+   
+  // Set CVT Mode
+  if (digitalRead(CVT_MODE_PIN)) {
+    if (PULL_MODE) {
+      PULL_MODE = false;
+    }
+    else {
+      PULL_MODE = true;
+    }
+  }
+
+  // Engage Stepper motor for either Manual or Pull Mode
+  if (PULL_MODE) {
+    if (PULL_OUT > 0) {
+      STEPPER.step(PULL_OUT, FORWARD, SINGLE); 
+    }
+    else if (PULL_OUT < 0) {
+      STEPPER.step(PULL_OUT, BACKWARD, SINGLE); 
+    }
+  }
+  else {
+    if (MANUAL_OUT > 0) {
+      STEPPER.step(MANUAL_OUT, FORWARD, SINGLE); 
+    }
+    else if (MANUAL_OUT < 0) {
+      STEPPER.step(MANUAL_OUT, BACKWARD, SINGLE); 
+    }
+  }
   
   // Output string buffer
   sprintf(DATA_BUFFER, "{'driveshaft':%d,'wheel':%d,'sparkplug':%d}", driveshaft_rpm, wheel_rpm, sparkplug_rpm);
@@ -96,22 +158,22 @@ int checksum() {
 
 /* --- ASYNCHRONOUS TASKS --- */
 void sparkplug_counter(void) {
-  sparkplug_pulses++;
+  SPARKPLUG_PULSES++;
 }
 
 void driveshaft_counter(void) {
-  driveshaft_pulses++;
+  DRIVESHAFT_PULSES++;
 }
 
 void wheel_counter(void) {
-  wheel_pulses++;
+  WHEEL_PULSES++;
 }
 
 void encoder_counter(void) {
   if (digitalRead(ENCODER_A_PIN) == digitalRead(ENCODER_B_PIN)) {
-    encoder_pulses++;
+    ENCODER_PULSES++;
   }
   else {
-    encoder_pulses--;
+    ENCODER_PULSES--;
   }
 }
