e2028be362e913311a14e02950376d8d685fa1ea
Trevor Stanhope <trevor@W510>
2015-04-05 23:21:15 -0400

    signiticantly cleaner and theoretically mature. needs live testing

--- sketches/VDC/VDC.ino
+++ sketches/VDC/VDC.ino
@@ -9,6 +9,7 @@
 #include <DualVNH5019MotorShield.h>
 
 /* --- GLOBAL CONSTANTS --- */
+// USB/CanBUS Info
 const char UID[] = "VDC";
 const int BAUD = 9600;
 const int DATA_SIZE = 128;
@@ -18,89 +19,115 @@ const int OUTPUT_SIZE = 256;
 const int STEERING_POSITION_PIN = A0;
 const int ACTUATOR_POSITION_PIN = A1;
 const int SUSPENSION_POSITION_PIN = A2;
-const int CART_POSITION_PIN = A3;
 
 // Digital Inputs
 const int CART_FORWARD_PIN = 2;
 const int CART_BACKWARD_PIN = 3;
+const int CART_MODE_PIN = 4;
 
 /* --- GLOBAL VARIABLES --- */
+// Create sensor values
 int LOAD_BALANCE_MODE = false;
 int STR_POS = 0;
 int ACT_POS = 0;
 int SUSP_POS = 0;
 int CART_POS = 0;
+boolean CART_FORWARD = false;
+boolean CART_BACKWARD = false;
 
+// Create character buffers
 char OUTPUT_BUFFER[OUTPUT_SIZE];
 char DATA_BUFFER[DATA_SIZE];
 
-double steering_set, steering_in, steering_out;
-double ballast_set, ballast_in, ballast_out;
-
-PID steering(&steering_in, &steering_out, &steering_set, 2, 5, 1, DIRECT);
-PID ballast(&ballast_in, &ballast_out, &ballast_set, 2, 5, 1, DIRECT);
+// Create PID controller objects
+double STEERING_SET, STEERING_IN, STEERING_OUT;
+double BALLAST_SET, BALLAST_IN, BALLAST_OUT;
+PID STEERING_PID(&STEERING_IN, &STEERING_OUT, &STEERING_SET, 2, 5, 1, DIRECT);
+PID BALLAST_PID(&BALLAST_IN, &BALLAST_OUT, &BALLAST_SET, 2, 5, 1, DIRECT);
 
 DualVNH5019MotorShield VDC; // M1 is Steering, M2 is Ballast
 
 /* --- SETUP --- */
 void setup() {
   Serial.begin(BAUD);
+  
+  // Initialize Inputs
+  pinMode(CART_FORWARD_PIN, INPUT);
+  pinMode(CART_BACKWARD_PIN, INPUT);
   pinMode(STEERING_POSITION_PIN, INPUT);
   pinMode(ACTUATOR_POSITION_PIN, INPUT);
   pinMode(SUSPENSION_POSITION_PIN, INPUT);
-  pinMode(CART_POSITION_PIN, INPUT);
-  ballast_set= 100;
-  steering_set = 100;
-  steering.SetMode(AUTOMATIC);
-  ballast.SetMode(AUTOMATIC);
+  
+  // TODO: Need a calibrated initial ballast/steering setpoints
+  BALLAST_SET = 0; 
+  STEERING_SET = 512;
+  STEERING_PID.SetMode(AUTOMATIC);
+  BALLAST_PID.SetMode(AUTOMATIC);
 }
 
 /* --- LOOP --- */
 void loop() {
     
+  /* --- START Steering Subsystem --- */
   // Set Steering
   STR_POS = analogRead(STEERING_POSITION_PIN);
   ACT_POS = analogRead(ACTUATOR_POSITION_PIN);
-  steering_in = double(ACT_POS);
-  steering_set = double(STR_POS);
-  steering.Compute();
+  STEERING_IN = double(ACT_POS);
+  STEERING_SET = double(STR_POS);
+  STEERING_PID.Compute();
+  
+  // Set steering actuator power output
   if (VDC.getM1CurrentMilliamps()) {
-    VDC.setM1Speed(int(steering_out));
+    VDC.setM1Speed(int(STEERING_OUT));
   }
   else {
     VDC.setM1Speed(0);
   }
+  /* --- END Steering Subsystem --- */
   
-  // Set Ballast
-  CART_POS = analogRead(CART_POSITION_PIN);
+  /* --- START Ballast Subsystm --- */
+  // Get Ballast system parameters and recompute PID
   SUSP_POS = analogRead(SUSPENSION_POSITION_PIN);
-  ballast.Compute();
+  CART_FORWARD = digitalRead(CART_FORWARD_PIN);
+  CART_BACKWARD = digitalRead(CART_BACKWARD_PIN);
+  BALLAST_IN = double(SUSP_POS);
+  BALLAST_PID.Compute();
+ 
+  // Set cart mode
+  if (digitalRead(CART_MODE_PIN)) {
+    if (LOAD_BALANCE_MODE) {
+      LOAD_BALANCE_MODE = false;
+    }
+    else {
+      LOAD_BALANCE_MODE = true;
+    }
+  }
+  
+  // Set ballast motor power
   if (LOAD_BALANCE_MODE) {
-    int val = int(ballast_out);
+    int val = int(BALLAST_OUT);
     VDC.setM2Speed(val);
   }
   else {
-    if (digitalRead(CART_FORWARD_PIN) && !(digitalRead(CART_BACKWARD_PIN))) {
-      VDC.setM2Speed(400);
+    if (CART_FORWARD && !CART_BACKWARD) {
+      VDC.setM1Speed(400);
     }
-    else if (digitalRead(CART_BACKWARD_PIN) && !(digitalRead(CART_FORWARD_PIN))) {
-      VDC.setM2Speed(-400);
+    else if (CART_BACKWARD && !CART_FORWARD) {
+      VDC.setM1Speed(-400);
     }
     else {
       VDC.setM2Speed(0);
     }
   }
-  
+  /* --- END Ballast Subsystem--- */
+
   // Format data buffer
   sprintf(DATA_BUFFER, "[str:%d,act:%d,cart:%d,susp:%d]", STR_POS, ACT_POS, CART_POS, SUSP_POS);
   
-  // format output to USB host {id, data, chksum}
+  // Format output to USB host by the following structure: {uid, data, chksum}
   sprintf(OUTPUT_BUFFER, "{'uid':%s,'data':%s,'chksum':%d}", UID, DATA_BUFFER, checksum());
   Serial.println(OUTPUT_BUFFER);
-  
-  // handle PID
-  steering.Compute();
-  ballast.Compute();
+ 
 }
 
 /* --- SYNCHRONOUS TASKS --- */
@@ -112,14 +139,4 @@ int checksum() {
   }
   int val = sum % 256;
   return val;
-}
-
-// Check Throttle Up
-void set_cart_backward(void) {
-  VDC.setM1Speed(-400);
-}
-
-// Check Throttle Down
-void set_cart_forward(void) {
-  VDC.setM1Speed(400);
 }
